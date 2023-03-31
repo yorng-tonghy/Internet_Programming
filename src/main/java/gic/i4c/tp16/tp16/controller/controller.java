082bd095819f942898af641a72ecb0d732a7b789
tonghy <tonghy@>
2023-03-31 19:33:00 +0700

    2nd commit

--- src/main/java/gic/i4c/tp16/tp16/controller/controller.java
+++ src/main/java/gic/i4c/tp16/tp16/controller/controller.java
@@ -3,27 +3,17 @@ package gic.i4c.tp16.tp16.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class controller {
-    @GetMapping(path = "/tp16/task1")
-    public ModelAndView task1(){
-        return new ModelAndView("task1");
+    @GetMapping(path = "/tp16")
+    public String index(){
+        return "<h1>hello</h1>";
     }
-    @GetMapping(path = "/tp16/task2")
+    @GetMapping(path="/tp16/hello")
     public ModelAndView task2(){
         return new ModelAndView("task2");
     }
-    @GetMapping(path = "/tp16/task3")
-    public ModelAndView task3(){
-        return new ModelAndView("task3");
-    }
-    @GetMapping(path = "/tp16/task4")
-    public ModelAndView task4(){
-        return new ModelAndView("task4");
-    }
-    @GetMapping(path = "/tp16/task5")
-    public ModelAndView task5(){
-        return new ModelAndView("task5");
-    }
+
 }
